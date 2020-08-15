55952463028a81ab521f3a75b13fec49f963a2c7
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-15 15:49:33 -0300

    Finished Crud Operations

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -2,9 +2,33 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 module.exports = {
-    async index(req, res) {
+    async users(req, res) {
         const Users = await User.find();
 
         return res.json(Users);
+    },
+    
+    async user(req, res) {
+        const user = await User.findById(req.params.id)
+    
+        return res.json(user);
+    },
+
+    async create_user(req, res) {
+        const newUser = await User.create(req.body);
+
+        return res.json(newUser);
+    },
+
+    async update_user(req, res) {
+        const updateUser = await User.findByIdAndUpdate(req.params.id, req.body, {new : true })
+    
+        return res.json(updateUser);
+    },
+
+    async delete_user(req, res) {
+        await User.findByIdAndRemove(req.params.id);
+
+        return res.send(`Successfully Removed User: ${req.params.id}`);
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const routes = express.Router();
 const UserController = require('./controllers/UserController');
 
-routes.get('/users', UserController.index);
+routes.get('/users', UserController.users);
+routes.get('/users/:id', UserController.user)
+routes.post('/users', UserController.create_user);
+routes.put('/users/:id', UserController.update_user);
+routes.delete('/users/:id', UserController.delete_user);
 
 module.exports = routes;
