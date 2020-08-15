685f4de583d2215aff99cd433a6990128fb5bc38
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-15 20:13:29 -0300

    Finished Styled Login Page

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
+import LoginImg from '../../Images/Login.svg'
+
+import { Wrapper, FirstContainer, SecondContainer } from './styles';
+
 function Login(){
     return(
-        <h1>Here Go Login Screen</h1>
+        <Wrapper>
+
+            <FirstContainer>
+
+                <h2>It´s Good To Have You Back</h2>
+
+                <img src={LoginImg} alt="It´s Good To Have You Back" />
+
+            </FirstContainer>
+
+            <SecondContainer>
+
+                <h2>Login Now</h2>
+
+                <form>
+
+                    <div className="formGroup">
+
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" placeholder="Name" id="name" />
+
+                    </div>
+
+                    <div className="formGroup">
+
+                        <label htmlFor="nickname">Nickname:</label>
+                        <input type="text" placeholder="Nickname" id="nickname" />
+
+                    </div>
+
+                    <button type="submit">Enter</button>   
+
+                </form>
+
+            </SecondContainer>
+
+        </Wrapper>
     )
 }
 
