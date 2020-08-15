c978999e0d2680e336533416a8f195ef4614f6ef
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-15 19:29:30 -0300

    Finished Welcome Screen

--- frontend/src/Pages/Welcome/index.js
+++ frontend/src/Pages/Welcome/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import WelcomeImg from '../../Images/Welcome.svg';
+
+import { Container, ButtonsContainer } from './styles';
 
 function Welcome(){
     return(
-        <h1>Welcome to My App</h1>
+        <Container>
+
+            <img src={WelcomeImg} alt="Seja Bem-Vindo(a)" />
+
+            <ButtonsContainer>
+
+                <Link 
+                    to="/login" 
+                    className="buttonLink" 
+                    id="login"
+                >
+
+                    Fazer Login
+
+                </Link>
+
+                <Link 
+                    to="/register" 
+                    className="buttonLink" 
+                    id="register"
+                >
+
+                    Cadastrar-se
+
+                </Link>
+
+            </ButtonsContainer>
+
+        </Container>
     )
 }
 
---
+++ frontend/src/Pages/Welcome/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color:#8356E5;
+    width:100vw;
+    height:100vh;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+
+    img{
+        width:60%;
+
+        @media (min-width:700px){
+            width:45%;
+        }
+
+        @media(min-width:1024px){
+            width:30%;
+        }
+    }
+`;
+
+export const ButtonsContainer = styled.div`
+    width:60%;
+    margin-top:60px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    
+    @media (min-width:1024px){
+        flex-direction:row;
+        justify-content:space-around;
+    }
+
+    .buttonLink{
+        width:100%;
+        height:54px;
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        text-decoration:none;
+        margin-top:30px;
+        border-radius:10px;
+        color:#fff;
+        font-size:18px;
+        cursor: pointer;
+        transition:0.4s;
+
+        @media (min-width:1024px){
+        width:45%;
+        height:90px;
+    }
+
+    }
+
+    #login{
+        background-color:#24134a;
+
+        &:hover{
+            background-color:#40345e;
+        }
+    }
+
+    #register{
+        background-color:#04d362;
+
+        &:hover{
+            background-color:#0da150;
+        }
+    }
+
+
+
+`;
