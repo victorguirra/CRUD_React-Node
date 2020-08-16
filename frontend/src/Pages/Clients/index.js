b173df68f19b628fea30d285596edf22a097d8f0
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-16 15:34:19 -0300

    Finished Styled Client Pages

--- frontend/src/Pages/Clients/index.js
+++ frontend/src/Pages/Clients/index.js
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+import api from '../../Services/api';
+
+import { Container, ClientsContainer, Option } from './styles';
 
 function Clients(){
+    const [ usersData, setUsersData ] = useState([]);
+
+    useEffect(() => {
+        async function FetchData(){
+            api.get('users')
+            .then(response => response.data)
+            .then(data => {
+                setUsersData(data.docs);
+            })
+        }
+        FetchData();
+    })
+
     return(
-        <h1>Here Go Clients Page</h1>
+        <Container>
+
+            <h2>Your Clients</h2>
+
+            <ClientsContainer>
+
+                {usersData.map( user => (
+                    <Option key={ user._id }>
+
+                        <div className="headerOption">
+
+                            <img src={ user.imageURL } alt={ user.name } />
+
+                            <div className="namesOption">
+
+                                <h3>{ user.nickname }</h3>
+
+                                <p>{ user.name }</p>
+
+                                <p>{ user.email }</p>
+
+                                <p>{ user.address }</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className="footerOptions">
+
+                            <Link id="updateButton" className="linkButton">Update</Link>
+
+                            <Link id="deleteButton" className="linkButton">Delete</Link>
+
+                        </div>
+
+                    </Option>
+                ))}
+
+            </ClientsContainer>
+
+        </Container>
     )
 }
 
---
+++ frontend/src/Pages/Clients/styles.js
@@ -0,0 +1,100 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color:#8356E5;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+
+    h2{
+        color:#fff;
+        margin-top:30px;
+    }
+`;
+
+export const ClientsContainer = styled.div`
+    width:40%;
+
+    @media (max-width:700px){
+        width:90%;
+    }
+    @media (min-width:701px) and (max-width:1024px){
+        width:75%;
+    }
+    @media (min-width:1025px) and (max-width:1220px){
+        width:50%;
+    }
+`;
+
+export const Option = styled.div`
+    display:flex;
+    flex-direction:column;
+    background-color:#fff;
+    margin-top:30px;
+    margin-bottom:20px;
+    border-radius:10px;
+    box-shadow:1px 1px 1px #000;
+    padding:20px;
+
+    .headerOption{
+        display:flex;
+        flex-direction:row;
+        align-items:center;
+        justify-content:space-around;
+
+        img{
+            width:120px;
+            height:120px;
+            border-radius:60px;
+        }
+    }
+
+    .namesOption{
+        width:60%;
+        display:flex;
+        flex-direction:column;
+        
+    }
+
+    .footerOptions{
+        margin-top:15px;
+        display:flex;
+        flex-direction:row;
+        align-items:center;
+        justify-content:space-around;
+
+        .linkButton{
+            color:#fff;
+            width:45%;
+            height:40px;
+            border-radius:8px;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            text-decoration:none;
+            transition:0.4s;
+        }
+
+        #updateButton{
+            background-color:#04d362;
+            
+            &:hover{
+                background-color:#0da150;
+            }
+        }
+
+        #deleteButton{
+            background-color:#ff1943;
+            
+
+            &:hover{
+                background-color:#d92e4c;
+            }
+        }
+
+    }
+
+`;
+
+
+
